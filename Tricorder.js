89b24f1dc4068009da92d4d306d52cd0991ee624
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2024-07-10 16:38:47 -0500

    Create Tricorder.js
    Makecode version of CircuitPlayground Tricorder

---
+++ Tricorder.js
@@ -0,0 +1,205 @@
+// advance mode, display signal color
+function nxtState () {
+    state = States.shift()
+    States.push(state)
+    if (state == "Light") {
+        light.setAll(0xffff00)
+    }
+    if (state == "Temp") {
+        light.setAll(0xff0000)
+    }
+    if (state == "Sound") {
+        light.setAll(0x00ff00)
+    }
+    if (state == "Text") {
+        light.setAll(0x0000ff)
+    }
+    pause(500)
+}
+// translate text, display in Morse
+function doCode (inputxt: string) {
+    encrypt(inputxt)
+    for (let index = 0; index <= code.length - 1; index++) {
+        char = code.substr(index, 1)
+        if (char == ".") {
+            dot()
+        }
+        if (char == "-") {
+            dash()
+        }
+        if (char == " ") {
+            space()
+        }
+    }
+}
+function encrypt (text: string) {
+    code = ""
+    for (let index2 = 0; index2 <= text.length - 1; index2++) {
+        char = text.substr(index2, 1)
+        enc = alphabet.indexOf(char)
+        if (-1 < enc) {
+            code = "" + code + morse[enc] + " "
+        } else {
+            code = "" + code + " "
+        }
+    }
+}
+function space () {
+    light.setAll(0x000000)
+    light.showRing(
+    `black black black purple purple purple purple black black black`
+    )
+    pause(100)
+    light.setAll(0x000000)
+}
+function dash () {
+    light.setAll(0x000000)
+    light.showRing(
+    `blue blue blue black black black black blue blue blue`
+    )
+    music.playTone(262, music.beat(BeatFraction.Half))
+    light.setAll(0x000000)
+}
+function dot () {
+    light.setAll(0x000000)
+    light.showRing(
+    `yellow black black black black black black black black yellow`
+    )
+    music.playTone(523, music.beat(BeatFraction.Quarter))
+    light.setAll(0x000000)
+}
+// translate number to string, display each digit with
+// neopixels.
+function ShowNum (value: number) {
+    pause2 = true
+    nstr = convertToText(value)
+    light.setAll(0x000000)
+    for (let index3 = 0; index3 <= nstr.length - 1; index3++) {
+        light.setAll(0x000000)
+        nval = parseFloat(nstr.substr(index3, 1))
+        if (nval == 0) {
+            light.setAll(0x00ffff)
+        } else {
+            for (let index22 = 0; index22 <= nval - 1; index22++) {
+                light.setPixelColor(index22, 0x00ff00)
+            }
+        }
+        pause(700)
+    }
+    pause(1000)
+    light.setAll(0x000000)
+    pause2 = false
+}
+function initMorse () {
+    morse = [".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..", ".----", "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----.", "-----", "/"]
+    alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", " "]
+    j316 = "for god so loved the world"
+    P23 = "the lord is my shepherd"
+    Phl4 = "i can do all things"
+    Isaiah263 = "thou wilt keep him in perfect peace"
+    _2Tim316 = "all scripture is given by inspiration of god"
+    gen11 = "in the beginning god created the heaven and the earth"
+    _1pt57 = "he careth for you"
+    i41 = "fear thou not"
+    verses = [Phl4, P23, _1pt57, i41, _2Tim316, gen11, Isaiah263, j316]
+    vnum = 0
+}
+// change mode
+input.buttonsAB.onEvent(ButtonEvent.Click, function () {
+    pause2 = true
+    light.setAll(0x000000)
+    nxtState()
+    if ("Text" == state) {
+        doCode("text")
+    }
+    pause2 = false
+})
+// initaie output appropriate to current state
+input.buttonA.onEvent(ButtonEvent.Click, function () {
+    if (state == "Light") {
+        ShowNum(input.lightLevel())
+    }
+    if (state == "Temp") {
+        if (input.switchRight()) {
+            ShowNum(input.temperature(TemperatureUnit.Celsius))
+        } else {
+            ShowNum(input.temperature(TemperatureUnit.Fahrenheit))
+        }
+    }
+    if (state == "Sound") {
+        ShowNum(input.soundLevel())
+    }
+    if (state == "Text") {
+        doCode(verses[vnum])
+    }
+})
+// choose text
+input.onGesture(Gesture.TiltRight, function () {
+    if ("Text" == state) {
+        vnum += 1
+        if (vnum >= verses.length) {
+            vnum = verses.length - 1
+        }
+        ShowNum(vnum)
+    }
+})
+// choose text 
+input.onGesture(Gesture.TiltLeft, function () {
+    if ("Text" == state) {
+        vnum += -1
+        if (vnum < 0) {
+            vnum = 0
+        }
+        ShowNum(vnum)
+    }
+})
+let vnum = 0
+let verses: string[] = []
+let i41 = ""
+let _1pt57 = ""
+let gen11 = ""
+let _2Tim316 = ""
+let Isaiah263 = ""
+let Phl4 = ""
+let P23 = ""
+let j316 = ""
+let nval = 0
+let nstr = ""
+let morse: string[] = []
+let alphabet: string[] = []
+let enc = 0
+let char = ""
+let code = ""
+let state = ""
+let pause2 = false
+let States: string[] = []
+States = ["Idle", "Light", "Temp", "Sound", "Text"]
+nxtState()
+pause2 = true
+initMorse()
+doCode("hello")
+pause2 = false
+forever(function () {
+    if (!(pause2) && state == "Idle") {
+        light.showAnimationFrame(light.rainbowAnimation)
+    }
+})
+forever(function () {
+    if (!(pause2) && state == "Light") {
+        light.graph(input.lightLevel())
+    }
+})
+forever(function () {
+    if (!(pause2) && state == "Temp") {
+        if (!(input.switchRight())) {
+            light.graph(input.temperature(TemperatureUnit.Fahrenheit))
+        } else {
+            light.graph(input.temperature(TemperatureUnit.Celsius))
+        }
+    }
+})
+forever(function () {
+    if (!(pause2) && state == "Sound") {
+        light.graph(input.soundLevel())
+    }
+})
